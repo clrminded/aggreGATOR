b2898095bab85de0ba2b63cc1088bec519b6c1bd
clrminded <the_rocket_scientist@icloud.com>
2024-09-24 21:54:41 -0700

    created textnode class file and test run in main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+from textnode import TextNode
+
+def main():
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(node.__repr__())
+
+if __name__ == "__main__":
+    main()
